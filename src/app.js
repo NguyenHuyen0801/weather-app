07a61146de97b6df56e8686edeb5f94855edf531
NguyenHuyen0801 <huyen7598@gmail.com>
2022-01-25 15:03:19 +0700

    change port

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require("./utils/forecast");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //define path for Express config
 const staticDirectoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
@@ -99,6 +101,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening in port 3000...");
+app.listen(port, () => {
+  console.log("Server is listening in port " + port + "...");
 });
