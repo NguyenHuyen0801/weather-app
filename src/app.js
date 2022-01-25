5d504e6dec6ff254c64da03a16a4f3b4752c9e8a
NguyenHuyen0801 <huyen7598@gmail.com>
2022-01-25 19:45:52 +0700

    test auto deploy heroku

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ app.get("/weather", (req, res) => {
 app.get("/products", (req, res) => {
   if (!req.query.search) {
     res.send({
-      error: "You must provide a search term",
+      error: "You must provide a search term!",
     });
   } else {
     console.log(req.query);
